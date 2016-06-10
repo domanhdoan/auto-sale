811c137ee6248b713e8769abd534b7a901a7d96f
manhdoan.do <doan.do@citigo.com.vn>
2016-06-10 15:40:55 +0700

    #21: remove duplicate in searching products

--- proccessor/product_finder.js
+++ proccessor/product_finder.js
@@ -56,7 +56,7 @@ function parse_keywords_calibration(keywords, word_list) {
 }
 
 function generate_shoesfind_query(keywords) {
-    var query = " Select P.id, P.title, P.price, P.thumbnail, P.code, P.link"
+    var query = " Select DISTINCT P.id, P.title, P.price, P.thumbnail, P.code, P.link"
         + " from product as P"
         + " inner join color as C on P.id = C.ProductId"
         + " inner join size as S on P.id = S.ProductId"
