9de3412c0dd23e76db9e78b96efae029082d45ae
DoanDM <manhdoan.do@gmail.com>
2016-07-17 00:10:41 +0700

    #63:  more friendly text

--- io/fbmessenger.js
+++ io/fbmessenger.js
@@ -79,7 +79,7 @@ function FBMessenger() {
             "image_url": thumbnail_url,
             "buttons": [{
                 "type": "postback",
-                "title": "Xem Ảnh SP & Size",
+                "title": "Nhấn xem Ảnh SP & Size",
                 "payload": JSON.stringify(payload1)
             }, {
                     "type": "postback",
