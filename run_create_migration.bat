073653df99d43d1a0f8944ce545f674e0459ec09
domanhdoan <manhdoan.do@gmail.com>
2016-08-05 17:52:55 +0700

    #79: remove folder for code. only keep sale document folder

--- run_create_migration.bat
+++
@@ -1,1 +0,0 @@
-.\node_modules\.bin\sequelize migration:create --name %1%
--- test/unittest/common-test.js
+++
@@ -1,7 +0,0 @@
-var chai = require('chai');
-var expect = chai.expect; // we are using the "expect" style of Chai
-
-var chai = require('../../util/common.js');
-
-
-
